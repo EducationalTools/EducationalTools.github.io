00c42e3cc2e1b30628edf4516d259f621db68966
Ingo (School Account) <117789688+Inglan2@users.noreply.github.com>
2024-05-29 06:24:33 +0000

    not sus

---
+++ script.js
@@ -0,0 +1,21 @@
+var a = 0
+
+function ee() {
+    a = a + 1;
+    if (a == 10) {
+        location = "sus"
+    }
+}
+
+function notsus() {
+    notsuselement = document.createElement("iframe");
+    notsuselement.src = "/sus";
+    notsuselement.style.border = "0";
+    notsuselement.sandbox = "allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation";
+    notsuselement.style.top = "0";
+    notsuselement.style.left = "0";
+    notsuselement.style.position = "fixed";
+    notsuselement.style.width = "100%";
+    notsuselement.style.height = "100%";
+    document.querySelector("body").appendChild(notsuselement);
+}
