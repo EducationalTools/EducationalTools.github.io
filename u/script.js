8804da95495554881fe70d39e3717e463a0ec848
Ingo (School Account) <0358381@schoolsnet.act.edu.au>
2024-07-02 11:54:38 +1000

    yay

--- u/script.js
+++ u/script.js
@@ -1,7 +1,12 @@
 fetch("config.json").then(function (r) {
     r.text().then(function (config) {
         JSON.parse(config).gmaes.forEach(gmae => {
-            console.log(gmae)
+            card = document.createElement("a")
+            card.innerHTML = gmae.name;
+            card.href = gmae.path;
+            card.style.backgroundImage = `url()`;
+            card.style.backgroundImage = (`url('${gmae.img}')`)
+            document.getElementById("gmaes").appendChild(card);
         });
     })
 })
