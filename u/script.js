494f3e7e191c2785595ab105211ccb523376d69a
Ingo (School Account) <117789688+Inglan2@users.noreply.github.com>
2024-07-25 21:09:54 +0000

    Update

--- u/script.js
+++ u/script.js
@@ -1,23 +1,19 @@
-setTimeout(() => {
-    if (window.location == window.parent.location) {
-        if (location.search != "?dev") {
-            location = "/";
-        }
-    }
-}, 1);
-
 fetch("config.json").then(function (r) {
-    r.text().then(function (config) {
-        JSON.parse(config).gmaes.forEach(gmae => {
-            card = document.createElement("a")
-            card.innerHTML = gmae.name;
-            card.href = gmae.path;
-            card.style.backgroundImage = `url()`;
-            card.style.backgroundImage = (`linear-gradient(to bottom, transparent, black), url('${gmae.img}')`);
-            card.classList.add("card")
-            ripple = document.createElement("md-ripple")
-            card.appendChild(ripple)
-            document.getElementById("gmaes").appendChild(card);
-        });
-    })
+    if ((window.location == window.parent.location) && (location.search !== "?dev")) {
+        location = "/";
+    } else {
+        r.text().then(function (config) {
+            JSON.parse(config).gmaes.forEach(gmae => {
+                card = document.createElement("a")
+                card.innerHTML = gmae.name;
+                card.href = gmae.path;
+                card.style.backgroundImage = `url()`;
+                card.style.backgroundImage = (`linear-gradient(to bottom, transparent, black), url('${gmae.img}')`);
+                card.classList.add("card")
+                ripple = document.createElement("md-ripple")
+                card.appendChild(ripple)
+                document.getElementById("gmaes").appendChild(card);
+            });
+        })
+    }
 })
