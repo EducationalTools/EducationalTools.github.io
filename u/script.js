446a47a9308bda17d7099529f484f7dca624d7e2
Ingo (School Account) <117789688+Inglan2@users.noreply.github.com>
2024-07-25 21:28:36 +0000

    Add click function placeholder

--- u/script.js
+++ u/script.js
@@ -12,6 +12,10 @@ fetch("config.json").then(function (r) {
             card = document.createElement("a")
             card.innerHTML = gmae.name;
             card.href = gmae.path;
+            card.onclick = function () {
+                alert();
+                return false;
+            };
             card.style.backgroundImage = `url()`;
             card.style.backgroundImage = (`linear-gradient(to bottom, transparent, black), url('${gmae.img}')`);
             card.classList.add("card")
